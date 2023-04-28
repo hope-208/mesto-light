74abceab345c26e4c59ff814fc310f00f0091d29
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2023-04-28 14:41:53 +0400

    refactor: ПР8, сборка с помощью Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    main: './src/index.js',
+    main: './pages/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -47,7 +47,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './index.html',
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
