0029fc1f0b4b2933ae3f932572b329be5d08487f
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2023-05-05 23:56:59 +0400

    refactor-js: ПР8, исправление замечаний ревью 1

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './index.html',
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
